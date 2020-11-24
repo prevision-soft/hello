5654ff69b865dcbfd8bb852b0a575184dbc403a9
bepul0 <bepul.cse.ru@gmail.com>
2020-11-24 11:54:17 +0600

    db modified

--- index.js
+++ index.js
@@ -7,32 +7,40 @@ const path = require('path');
 require('dotenv').config();
 require('./services/passport.js')
 const compression = require('compression')
-const config = require('./config.js');
+//const config = require('./config.js');
 
-const env = process.env.NODE_ENV || 'development';
+//const env = process.env.NODE_ENV || 'development';
 
-mongoose.connect(env === 'development' ? config.DB_URI_DEV : config.DB_URI, {useUnifiedTopology: true, useNewUrlParser: true})
+//mongoose.connect(env === 'development' ? config.DB_URI_DEV : config.DB_URI, {useUnifiedTopology: true, useNewUrlParser: true})
 
-// mongoose.connect(
-//   env.MONGO_URI || "mongodb://localhost/internship",
-//   {
-//     useNewUrlParser: true,
-//   },
-//   console.log("Mongodb connected")
-// );
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost/internship",
+  {
+    useNewUrlParser: true,
+  },
+  console.log("Mongodb connected")
+);
 
 const app = express()
 app.use(compression())
 app.use(bodyParser.urlencoded({extended:true}))
-env !== 'development' && app.use(express.static(path.join(__dirname, 'client/build')));
+//env !== 'development' && app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-env === 'development' && app.use(morgan('dev'))
+//env === 'development' && app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use('/api', require('./routes/router'))
 
-env !== 'development' && app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// env !== 'development' && app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
 
 app.listen(process.env.PORT || 5000)
